db9cb02aaf2ce4dce935d4778421f0f61fd392c5
elakyc15 <e.f.d.koycu@student.vu.nl>
2025-06-04 14:53:49 +0200

    voorbeeld_data_opslaan

---
+++ inlezendata.R
@@ -0,0 +1,3 @@
+
+Inkomen2018ArnhemNL <- read.csv("~/Library/CloudStorage/OneDrive-VrijeUniversiteitAmsterdam/PFE Group 7/Kerncijfers_wijken_en_buurten_2018_04062025_142520.csv", sep=";")
+write.csv(Inkomen2018ArnhemNL, "data/Inkomen2018ArnhemNL.csv")
