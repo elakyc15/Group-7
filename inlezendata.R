76b19ddade17755faf97b7c0824600dc5773c7a6
elakyc15 <e.f.d.koycu@student.vu.nl>
2025-06-04 15:16:48 +0200

    Update inlezendata.R

--- inlezendata.R
+++ inlezendata.R
@@ -1,3 +1,12 @@
 
 Inkomen2018ArnhemNL <- read.csv("~/Library/CloudStorage/OneDrive-VrijeUniversiteitAmsterdam/PFE Group 7/Kerncijfers_wijken_en_buurten_2018_04062025_142520.csv", sep=";")
 write.csv(Inkomen2018ArnhemNL, "data/Inkomen2018ArnhemNL.csv")
+
+Inkomen2022AmsterdamNL <- read.csv("~/Library/CloudStorage/OneDrive-VrijeUniversiteitAmsterdam/PFE Group 7/Kerncijfers_wijken_en_buurten_2022_04062025_142936.csv", sep=";")
+write.csv(Inkomen2022AmsterdamNL, "data/Inkomen2022AmsterdamNL.csv")
+
+Inkomen2022ArnhemNL <- read.csv("~/Library/CloudStorage/OneDrive-VrijeUniversiteitAmsterdam/PFE Group 7/Kerncijfers_wijken_en_buurten_2022_04062025_142903.csv", sep=";")
+write.csv(Inkomen2022ArnhemNL, "data/Inkomen2022ArnhemNL.csv")
+
+Inkomen2018AmsterdamNL <- read.csv("~/Library/CloudStorage/OneDrive-VrijeUniversiteitAmsterdam/PFE Group 7/Kerncijfers_wijken_en_buurten_2018_04062025_142639.csv", sep=";")
+write.csv(Inkomen2018AmsterdamNL, "data/Inkomen2018AmsterdamNL.csv")
