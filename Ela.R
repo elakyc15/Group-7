fcccad258d60f0b473d068eed0e9411cb68a4bb6
elakyc15 <e.f.d.koycu@student.vu.nl>
2025-06-11 13:59:45 +0200

    E west en K zuid naar west en zuid vernoemd

--- Ela.R
+++ Ela.R
@@ -1,4 +1,4 @@
-write.csv(Kerncijfers_wijken_en_buurten_2019_05062025_112613, "data/Kerncijfers_wijken_en_buurten_2019.csv")
+#write.csv(Kerncijfers_wijken_en_buurten_2019_05062025_112613, "data/Kerncijfers_wijken_en_buurten_2019.csv")
 
 # 1. Laad de dataset
 huur <- read.csv("data/huurprijzen.csv", skip = 1, header = FALSE)
@@ -13,22 +13,28 @@ colnames(huur) <- c("Index", "Stadsdeel", "2013_p", "2015_p", "2017_p", "2019_p"
 # 4. Selecteer alleen particuliere huurprijzen (kolommen eindigend op "_p")
 huur_particulier <- huur[, c("Stadsdeel", "2013_p", "2015_p", "2017_p", "2019_p")]
 
+library(dplyr)
+
+huur_particulier <- huur_particulier %>%
+  mutate(Stadsdeel = recode(Stadsdeel, "E West" = "West", "K Zuid" = "Zuid"))
+
 # 5. Print resultaat
 print(huur_particulier)
 
 library(tidyverse)
 
 # Stap 2: Filter alleen op E West en K Zuid (particuliere huurprijzen)
-huur <- huur %>%
-  filter(V2 %in% c("E West", "K Zuid")) %>%
-  select(V2, V5, V6)  # V5 = 2017 (particulier), V6 = 2019 (particulier)
+#huur <- huur %>%
+#  filter(V2 %in% c("E West", "K Zuid")) %>%
+#  select(V2, V5, V6)  # V5 = 2017 (particulier), V6 = 2019 (particulier)
 
 # Stap 3: Hernoem kolommen
-colnames(huur) <- c("stadsdeel", "2017", "2019")
+#colnames(huur) <- c("stadsdeel", "2017", "2019")
+
 
 # Stap 4: Pivot naar lange vorm
 huur_lang <- huur %>%
-  pivot_longer(cols = c("2017", "2019"),
+  pivot_longer(cols = c("2017_p", "2019_p"),
                names_to="jaar",
                values_to = "huurprijs")
 
